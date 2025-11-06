6e94bfff44d7e9f3f119b08a70213b587c4d8693
Mihai Fufezan <mihai@fufexan.net>
2025-11-06 23:37:57 +0200

    nix: init

---
+++ flake.nix
@@ -0,0 +1,89 @@
+{
+  description = "Hyprland GUI utilities (successor to hyprland-qtutils)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default-linux";
+
+    aquamarine = {
+      url = "github:hyprwm/aquamarine";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+        hyprutils.follows = "hyprutils";
+      };
+    };
+
+    hyprgraphics = {
+      url = "github:hyprwm/hyprgraphics";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+        hyprutils.follows = "hyprutils";
+      };
+    };
+
+    hyprlang = {
+      url = "github:hyprwm/hyprlang";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+        hyprutils.follows = "hyprutils";
+      };
+    };
+
+    hyprutils = {
+      url = "github:hyprwm/hyprutils";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+      };
+    };
+
+    hyprtoolkit = {
+      url = "github:hyprwm/hyprtoolkit";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+        aquamarine.follows = "aquamarine";
+        hyprgraphics.follows = "hyprgraphics";
+        hyprlang.follows = "hyprlang";
+        hyprutils.follows = "hyprutils";
+      };
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+      ...
+    }@inputs:
+    let
+      inherit (nixpkgs) lib;
+      eachSystem = lib.genAttrs (import systems);
+      pkgsFor = eachSystem (
+        system:
+        import nixpkgs {
+          localSystem = system;
+          overlays = [ self.overlays.default ];
+        }
+      );
+    in
+    {
+      overlays = import ./nix/overlays.nix { inherit inputs self lib; };
+
+      packages = eachSystem (system: {
+        default = self.packages.${system}.hyprland-guiutils;
+        inherit (pkgsFor.${system}) hyprland-guiutils;
+      });
+
+      devShells = eachSystem (system: {
+        default = import ./nix/shell.nix {
+          pkgs = pkgsFor.${system};
+          inherit (pkgsFor.${system}) hyprland-guiutils;
+        };
+      });
+    };
+}
---
+++ nix/default.nix
@@ -0,0 +1,57 @@
+{
+  lib,
+  stdenv,
+  cmake,
+  pkg-config,
+  aquamarine,
+  cairo,
+  hyprgraphics,
+  hyprlang,
+  hyprtoolkit,
+  hyprutils,
+  libdrm,
+  pixman,
+  version ? "0",
+}:
+let
+  inherit (lib.sources) cleanSource cleanSourceWith;
+  inherit (lib.strings) hasSuffix;
+in
+stdenv.mkDerivation {
+  pname = "hyprland-guiutils";
+  inherit version;
+
+  src = cleanSourceWith {
+    filter =
+      name: _type:
+      let
+        baseName = baseNameOf (toString name);
+      in
+      !(hasSuffix ".nix" baseName);
+    src = cleanSource ../.;
+  };
+
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+  ];
+
+  buildInputs = [
+    aquamarine
+    cairo
+    hyprgraphics
+    hyprlang
+    hyprtoolkit
+    hyprutils
+    libdrm
+    pixman
+  ];
+
+  meta = {
+    description = "Hyprland GUI utilities (successor to hyprland-guiutils)";
+    homepage = "https://github.com/hyprwm/hyprland-qtlibs";
+    license = lib.licenses.bsd3;
+    maintainers = [ lib.maintainers.fufexan ];
+    platforms = lib.platforms.linux;
+  };
+}
